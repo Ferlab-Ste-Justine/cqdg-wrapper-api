924aae9d7200e323403ba0bbc7bd1b7a8fa77368
Adrian <adipaul1981@yahoo.ca>
2025-01-10 17:05:24 -0500

    fix: CQDG-947 update mapping

--- src/graphql/file/types/file.ts
+++ src/graphql/file/types/file.ts
@@ -25,7 +25,7 @@ export const FileType = new GraphQLObjectType({
     id: { type: GraphQLString },
     file_id: { type: GraphQLString },
     file_2_id: { type: GraphQLString },
-    biospecimen_reference: { type: GraphQLString },
+    biospecimen_reference: { type: new GraphQLList(GraphQLString) },
     data_category: { type: GraphQLString },
     data_type: { type: GraphQLString },
     dataset: { type: GraphQLString },
